1eb18a5fd3cec571599c8dc261dd9d13847ef279
aocabbage <acabbage@uw.edu>
2025-11-01 17:43:50 -0700

    Fix overflow

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${lato.className} antialiased bg-primary-50 text-primary-900`}
+        className={`${geistSans.variable} ${geistMono.variable} ${lato.className} overflow-x-hidden antialiased bg-primary-50 text-primary-900`}
       >
         <Navbar/>
         {children}
